b8faee04a4b43fee8ae35f980404f02db785acee
Paulo Ricardo Ziemer dos Santos <PauloZiemer.private@gmail.com>
2023-02-24 12:54:11 -0300

    Novamente.

--- Projeto-Integrador/pages/Redes.xaml.cs
+++ Projeto-Integrador/pages/Redes.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Projeto-Integrador.pages
+namespace Projeto_Integrador.pages
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 public partial class Redes : ContentPage
