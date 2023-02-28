de234663f801f54aa5cdaf9d72c15527cde6780b
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2023-02-28 13:56:19 -0300

    Adicionado duas imagens de teste para os botões.

--- Projeto-Integrador.Android/Resources/Resource.designer.cs
+++ Projeto-Integrador.Android/Resources/Resource.designer.cs
@@ -16682,13 +16682,19 @@ namespace Projeto_Integrador.Droid
 			public const int shellWin = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int test_custom_background = 2131165334;
+			public const int subtopico1 = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int tooltip_frame_dark = 2131165335;
+			public const int subtopico2 = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int tooltip_frame_light = 2131165336;
+			public const int test_custom_background = 2131165336;
+			
+			// aapt resource value: 0x7F070099
+			public const int tooltip_frame_dark = 2131165337;
+			
+			// aapt resource value: 0x7F07009A
+			public const int tooltip_frame_light = 2131165338;
 			
 			static Drawable()
 			{
