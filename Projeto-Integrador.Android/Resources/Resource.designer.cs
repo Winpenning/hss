4aed3d2eb5c04af3baf62880a5559f37584d6334
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2023-03-02 13:54:18 -0300

    Protótipo do primeiro ImageButton em redes.xaml

--- Projeto-Integrador.Android/Resources/Resource.designer.cs
+++ Projeto-Integrador.Android/Resources/Resource.designer.cs
@@ -16682,31 +16682,28 @@ namespace Projeto_Integrador.Droid
 			public const int notify_panel_notification_icon_bg = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int preference_list_divider_material = 2131165334;
+			public const int osimodel = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int shellLin = 2131165335;
+			public const int preference_list_divider_material = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int shellWin = 2131165336;
+			public const int shellLin = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int subtopico1 = 2131165337;
+			public const int shellWin = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int subtopico2 = 2131165338;
+			public const int terminalLin = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int terminalLin = 2131165339;
+			public const int test_custom_background = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int test_custom_background = 2131165340;
+			public const int tooltip_frame_dark = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int tooltip_frame_dark = 2131165341;
-			
-			// aapt resource value: 0x7F07009E
-			public const int tooltip_frame_light = 2131165342;
+			public const int tooltip_frame_light = 2131165341;
 			
 			static Drawable()
 			{
