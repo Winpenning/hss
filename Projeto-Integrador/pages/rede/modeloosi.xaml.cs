6630153565a509c717a47eaad7dd16a2e0fead27
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2023-03-02 14:00:53 -0300

    modeloosi.xaml criado

---
+++ Projeto-Integrador/pages/rede/modeloosi.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Projeto_Integrador.pages.rede
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class modeloosi : ContentPage
+	{
+		public modeloosi ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
