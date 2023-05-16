c30f2f1cfba0c8c3d6f1656d1b9a1b812c6aed95
winpenning <zs.phenriqueprofessional@gmail.com>
2023-05-16 19:33:06 -0300

    Página de usuários e grupos criada

---
+++ Projeto-Integrador/pages/linux/usuario.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Projeto_Integrador.pages.linux
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class usuario : ContentPage
+	{
+		public usuario ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
