934725bc3ffbbdc152902a001a2b5b659ba8c553
Winpenning <zs.phenriqueprofessional@gmail.com>
2023-06-20 18:42:01 -0300

    Página sobre.xaml criada

---
+++ Projeto-Integrador/pages/sobre.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Projeto_Integrador.pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class sobre : ContentPage
+	{
+		public sobre ()
+		{
+			InitializeComponent ();
+            this.BindingContext = new MainPageViewModel();
+        }
+    }
+}
