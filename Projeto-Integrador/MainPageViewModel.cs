4a7a6b1da66141dffe3ce8ea6d699428be37cc77
Winpenning <zs.phenriqueprofessional@gmail.com>
2023-06-20 18:43:44 -0300

    Classe MainPageViewModel criada

---
+++ Projeto-Integrador/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Windows.Input;
+using Xamarin.Forms;
+namespace Projeto_Integrador
+{
+    public class MainPageViewModel
+    {
+        public ICommand ClickCommand => new Command<string>((url) =>
+        {
+            Device.OpenUri(new System.Uri(url));
+        });
+    }
+}
