fd7e554aa06d2e039bea71af344543fc1f617a2b
Winpenning <zs.phenriqueprofessional@gmail.com>
2023-06-21 13:10:21 -0300

    Device.OpenUri trocado pro Laucher.OpenAsync

--- Projeto-Integrador/MainPageViewModel.cs
+++ Projeto-Integrador/MainPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 namespace Projeto_Integrador
 {
@@ -7,7 +8,9 @@ namespace Projeto_Integrador
     {
         public ICommand ClickCommand => new Command<string>((url) =>
         {
-            Device.OpenUri(new System.Uri(url));
+           // Device.OpenUri(new System.Uri(url));
+           // Device.OpenUri está obsoleto
+            Launcher.OpenAsync(new System.Uri(url));
         });
     }
 }
